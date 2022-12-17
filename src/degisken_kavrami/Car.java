742fbb05811fc90ed3c7c98ada6ed3b1acb05efd
Furkan Akcan <akcanfurkan@hotmail.com.tr>
2022-12-18 00:30:11 +0300

    created: Car.java

---
+++ src/degisken_kavrami/Car.java
@@ -0,0 +1,37 @@
+package degisken_kavrami;
+
+public class Car {
+
+    public String make;
+    public String model;
+    public String year;
+    public int speed;
+    public int distance;
+
+    public double go(int newDistance) {
+
+        distance += newDistance;
+        double period = (double) newDistance / speed;
+        return period;
+
+
+    }
+
+    public void accelerate(int newSpeed) {
+
+        speed = newSpeed;
+
+    }
+
+    public void stop() {
+        speed = 0;
+    }
+
+
+    public String getInfo() {
+        String info = "Car Info: " + year + " " + make + " " + model + " " +
+                ". Distance: " + distance + " km. and travelling at " + speed + " kmph. ";
+        return info;
+    }
+
+}
