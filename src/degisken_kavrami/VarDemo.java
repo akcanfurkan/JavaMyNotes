b48e1622d2a670c8f483d11e6c04422ed1792d34
Furkan Akcan <akcanfurkan@hotmail.com.tr>
2022-12-19 00:10:38 +0300

    created file: VarDemo

---
+++ src/degisken_kavrami/VarDemo.java
@@ -0,0 +1,19 @@
+package degisken_kavrami;
+
+import referans_kavrami.Cat;
+
+public class VarDemo {
+
+    public static void main(String[] args) {
+
+        var i = 5;
+        var a = 'a';
+        var isClosed = false;
+        var b = 1.2;
+        var c = i + b;
+        System.out.println(c);
+
+        var s1 = new Car();
+
+    }
+}
