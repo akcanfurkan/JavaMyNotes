10504719d875230801c3c2c12c8873ecba74d67d
Furkan Akcan <akcanfurkan@hotmail.com.tr>
2022-12-25 23:54:29 +0300

    Created.java

---
+++ src/degisken_kavrami/Exercise1bA.java
@@ -0,0 +1,18 @@
+package degisken_kavrami;
+
+public class Exercise1bA {
+
+
+        public static void main(String[] args) {
+
+            int x = 1;
+
+            while (x < 10) {
+                if (x > 3) {
+                    System.out.println("Big x!");
+                }
+            }
+        }
+    }
+
+
