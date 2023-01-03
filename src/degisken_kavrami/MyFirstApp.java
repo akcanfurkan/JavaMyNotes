88a1bdd1356dae5004795781dcb1664eb796f977
Furkan Akcan <akcanfurkan@hotmail.com.tr>
2023-01-04 00:21:30 +0300

    new file: MyFirstApp

---
+++ src/degisken_kavrami/MyFirstApp.java
@@ -0,0 +1,11 @@
+package degisken_kavrami;
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("I Rule!");
+        System.out.println("The World...");
+    }
+
+}
